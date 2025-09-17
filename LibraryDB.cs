808734b5b9608088d3085fb4cd69e248ca66f9ce
USSSH <usama.shahla9@gmail.com>
2025-09-17 12:17:34 +0300

    Update Book method

--- LibraryDB.cs
+++ LibraryDB.cs
@@ -55,7 +55,7 @@ public class LibraryDB
 
         connection.Close();
     }
-     public void AddingABook(string title, string author, string category)
+    public void AddingABook(string title, string author, string category)
     {
         using (var connection = new SqliteConnection(_connectionString))
         {
@@ -83,5 +83,26 @@ public class LibraryDB
 
         }
     }
+
+    public void UpdateBook(string title, string newAuthor, string newCategory)
+    {
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+            var commandForUpdate = connection.CreateCommand();
+            commandForUpdate.CommandText = @"
+            UPDATE Books 
+            SET author = @Author, category = @Category 
+            WHERE title = @Title";
+
+            commandForUpdate.Parameters.AddWithValue("@Author", newAuthor);
+            commandForUpdate.Parameters.AddWithValue("@Category", newCategory);
+            commandForUpdate.Parameters.AddWithValue("@Title", title);
+
+            int rowsAffected = commandForUpdate.ExecuteNonQuery();
+
+        }
+    }
+
 }
 
